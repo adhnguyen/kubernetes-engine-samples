e59b4089574de09a9bdd9b6b505fd956b4307eed
Anh Nguyen <anh@poly-ai.com>
2024-01-22 10:10:16 +0000

    add weather EP

--- quickstarts/hello-app/main.go
+++ quickstarts/hello-app/main.go
@@ -19,26 +19,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
+
+type WeatherRequest struct {
+	Lat    float64 `json:"lat"`
+	Lon    float64 `json:"lon"`
+	ApiKey string  `json:"apiKey"`
+}
 
 func main() {
 	// register hello function to handle all requests
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/", hello)
+	// mux.Handle("/weather", http.HandlerFunc(weather)).Methods("POST")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", hello)
+	r.HandleFunc("/weather", weather).Methods("POST")
 
 	// use PORT environment variable, or default to 8080
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "6068"
 	}
 
 	// start the web server on port and accept requests
 	log.Printf("Server listening on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
 // hello responds to the request with a plain-text "Hello, world" message.
@@ -48,6 +62,68 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
+}
+
+func weather(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+
+	decoder := json.NewDecoder(r.Body)
+	var requestData WeatherRequest
+
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		message := fmt.Sprintf("Error decoding JSON request: %s", err)
+		fmt.Fprintf(w, `{"error": "%s"}`, message)
+		return
+	}
+
+	lat := requestData.Lat
+	lon := requestData.Lon
+	apiKey := requestData.ApiKey
+
+	response, err := http.Get(fmt.Sprintf(
+		"https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&appid=%s",
+		lat, lon, apiKey))
+
+	if err != nil {
+		message := fmt.Sprintf("Failed to call API: %s", err)
+		fmt.Fprintf(w, `{"error": "%s"}`, message)
+		return
+	}
+	defer response.Body.Close()
+
+	var responseJson map[string]interface{}
+	json.NewDecoder(response.Body).Decode(&responseJson)
+
+	if response.StatusCode != 200 {
+		message := "Failed to call API."
+		if errMsg, ok := responseJson["message"].(string); ok {
+			message += " Error: " + errMsg
+		}
+		fmt.Fprintf(w, `{"error": "%s"}`, message)
+		return
+	}
+
+	summary := responseJson["weather"].([]interface{})[0].(map[string]interface{})["description"].(string)
+
+	// Return JSON response
+	jsonResponse := map[string]interface{}{
+		"sessionInfo": map[string]interface{}{
+			"parameters": map[string]interface{}{
+				"summary": summary,
+			},
+		},
+	}
+
+	jsonData, err := json.Marshal(jsonResponse)
+	if err != nil {
+		message := fmt.Sprintf("Error encoding JSON response: %s", err)
+		fmt.Fprintf(w, `{"error": "%s"}`, message)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
 
 // [END container_hello_app]
